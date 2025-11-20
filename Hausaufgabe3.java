f8a34f707c265d276a502d42e9ded4ccc555f34c
Kimmie-code <postnikowdaniel@gmail.com>
2025-11-20 11:16:43 +0100

    Stand 20.11.2025

---
+++ Hausaufgabe3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Hausaufgabe3{
+public static void main(String [] args){
+        Scanner Keyb = new Scanner(System.in);
+        float TU, HTU;
+        HTU = 0;
+
+        do{
+            System.out.println("Tagesumsatz eingeben: "); TU = Keyb.nextFloat();
+            if (TU > HTU){
+                HTU = TU;
+            }
+        }
+            while (TU >= 0);
+
+            System.out.print("Hoechster Tagesumsatz: " + HTU + " Euro");
+        
+    }
+}
