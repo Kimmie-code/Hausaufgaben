b78e554b4858a610f919cd48a5816e44959cdac5
Kimmie-code <postnikowdaniel@gmail.com>
2025-12-01 10:08:05 +0100

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,12 @@
+public class test1 {
+    public static void main (String [] args){
+        int test = 0;
+
+        while (test<10){
+            System.out.println("Die Variable Test hat den wert" + test);
+            test = test + 1;
+        }
+        
+        System.out.println("Hallo");
+    }
+}
