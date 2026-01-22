9cc9244edec3ba6b387d5f90e844bf946cd6d251
Kimmie-code <postnikowdaniel@gmail.com>
2026-01-22 09:30:42 +0100

    stand 22.01.2026

---
+++ connect4.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class connect4 {
+    public static void main(String[]args){
+        Scanner Keyb = new Scanner(System.in);
+        char board[][] = new char [6][7];
+        char x = 'X', o = 'O';
+
+
+         for(int z = 0; z < 6; z++ ){
+            for(int s = 0; s < 7; s++){
+                board [z][s] = '.';
+            }
+         }
+
+            System.out.println();
+            System.out.println("********** Spiel 4-Gewint **********");
+            System.out.println("Spielfeld:");
+            System.out.println();
+            System.out.println("    1   2   3   4   5   6   7  ");
+            System.out.println("  -----------------------------");
+
+            for(int z = 0; z < 6; z++){
+                System.out.print((z+1) + " | ");
+                for(int s = 0; s < 7; s++){
+                    System.out.print(board [z][s] + " | ");
+                }
+                System.out.println();
+                System.out.println("  -----------------------------");
+            }
+        }
+    }
+    
+
