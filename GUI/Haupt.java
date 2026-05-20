4f112cdcbea762ac24cae73af9b3020f8e35f1ba
Kimmie-code <postnikowdaniel@gmail.com>
2026-05-20 12:38:50 +0200

    A

---
+++ GUI/Haupt.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+class Formular extends Frame{
+    
+}
+public class Haupt {
+    public static void main(String args[]){
+        Formular F1 = new Formular ();
+        
+        F1.setVisible(true);
+    }
+}
